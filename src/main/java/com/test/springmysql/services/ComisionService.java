71364f03e9fdf625b76083dbbd2518bda8b937cc
luciano <lucianorodriguez1@users.noreply.github.com>
2025-11-19 01:19:32 -0300

    agregar estudiante a comision. dejar listo para escalar el proyecto

--- src/main/java/com/test/springmysql/services/ComisionService.java
+++ src/main/java/com/test/springmysql/services/ComisionService.java
@@ -2,8 +2,10 @@ package com.test.springmysql.services;
 
 import com.test.springmysql.dtos.ComisionDTO;
 import com.test.springmysql.entities.Comision;
+import com.test.springmysql.entities.Estudiante;
 import com.test.springmysql.exceptions.RecursoNoEncontrado;
 import com.test.springmysql.repositories.ComisionRepository;
+import com.test.springmysql.repositories.EstudianteRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +14,25 @@ import java.util.List;
 @Service
 public class ComisionService {
     private final ComisionRepository comisionRepository;
+    private final EstudianteRepository estudianteRepository;
+
     ModelMapper mapper = new ModelMapper();
 
-    public ComisionService(ComisionRepository comisionRepository) {
+    public ComisionService(ComisionRepository comisionRepository, EstudianteRepository estudianteRepository) {
         this.comisionRepository = comisionRepository;
+        this.estudianteRepository = estudianteRepository;
     }
 
     public List<ComisionDTO> getComisiones(){
         return comisionRepository.findAll().stream()
-                .map(c -> mapper.map(c, ComisionDTO.class)).toList();
+                .map(c -> {
+                    ComisionDTO dto = mapper.map(c, ComisionDTO.class);
+
+                    dto.setEstudiantesId(
+                            c.getEstudiantes().stream().map(e->e.getId()).toList()
+                    );
+                    return dto;
+                }).toList();
     }
     public ComisionDTO getComision(Long id){
        Comision c = comisionRepository.findById(id)
@@ -50,5 +62,18 @@ public class ComisionService {
         Comision saved = comisionRepository.save(c);
         return mapper.map(saved, ComisionDTO.class);
     }
+
+    public void addStudent(Long cid, Long eid){
+        Comision c = comisionRepository.findById(cid)
+                .orElseThrow(()-> new RecursoNoEncontrado("comision","id",cid));
+        Estudiante e = estudianteRepository.findById(eid)
+                .orElseThrow(()-> new RecursoNoEncontrado("estudiante","id",eid));
+
+        c.getEstudiantes().add(e);
+        comisionRepository.save(c);
+    }
+
+
+
 
 }
